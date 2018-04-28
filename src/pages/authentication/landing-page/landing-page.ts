3ec1a6d7190f5305f1af6fdbf2b34dccfdfd2d9b
zainasaed <zainushka11@gmail.com>
2018-04-28 14:47:25 +0300

    zaina's edit on register

--- src/pages/authentication/landing-page/landing-page.ts
+++ src/pages/authentication/landing-page/landing-page.ts
@@ -1,16 +1,22 @@
 /** Represents a Component of landing page. */
+import { Platform, IonicPage } from 'ionic-angular';
 
 /** Imports Modules */
 import { Component } from '@angular/core';
-import { NavController, IonicPage, MenuController } from 'ionic-angular';
+import { NavController,  MenuController } from 'ionic-angular';
+import { TranslateService } from 'ng2-translate';
+import { SettingsService } from '../../settings/settings.service';
 
 @IonicPage()
 @Component({
   selector: 'landing-page',
-  templateUrl: 'landing-page.html'
+  templateUrl: 'landing-page.html',
+  providers: [SettingsService]
 })
 export class LandingPageComponent {
-  constructor(public navCtrl: NavController,
+  
+  constructor( public translate:
+    TranslateService, public navCtrl: NavController,
     private menu: MenuController) {
     this.menu.enable(false); // Disable sidemenu
   }
@@ -19,4 +25,9 @@ export class LandingPageComponent {
     console.log(component)
     this.navCtrl.setRoot(component);
   }
+  public changeLanguage(language){
+    this.translate.use(language);
+  }
+
+  
 }
